89a83044cfa957a7217bdb496a5cbe418e8c0c95
Jayesh B <55828915+Jayesh-Borse@users.noreply.github.com>
2021-07-10 16:24:19 +0530

    update stockCharts.js

--- src/models/stockCharts.js
+++ src/models/stockCharts.js
@@ -13,7 +13,7 @@ function chartHandler(message, msg){
         companyMonthlyChart(message, msg[1]);
     }
     else if(msg[2] === "D"){
-
+        companyIntradayChart(message, msg[1]);
     }
     else if(msg[2] === "Y"){
 
@@ -58,6 +58,8 @@ async function getMonthlyData(companySymbol) {
                     break;
             }
     });
+    dataDict["labels"].reverse();
+    dataDict["dataset"].reverse();
     return dataDict;
 }
 
@@ -115,6 +117,108 @@ async function companyMonthlyChart(message, companySymbol){
     const chartMessage = new Discord.MessageEmbed()
         .setColor('#00FF00')
         .setTitle(companySymbol + " (Stock Price - Last 30 days)")
+        .setImage(myChart.getUrl())
+        .setTimestamp()
+    message.reply(chartMessage);
+    console.log(myChart.getUrl());
+}
+
+async function getIntradayData(companySymbol){
+    url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol="+companySymbol+"&interval=15min&apikey="+ process.env.API_KEY;
+    var dataDict = {
+        "metadata" : [],
+        "labels" : [],
+        "dataset" : []
+    }
+    await axios({
+        method : 'get',
+        url: url,
+        responseType: json,
+      })
+       .then((res) => {
+            //console.log(res['data']['Time Series (Daily)']);
+            var dailyData = res['data']['Time Series (15min)']
+            var date = res['data']['Meta Data']['3. Last Refreshed'].split(' ')[0]
+            dataDict["metadata"].push(date)
+            // console.log(dailyData)
+            // console.log(res)
+            i = 0;
+            for (let key of Object.keys(dailyData)) {
+                var time = key.split(' ')[1].split(':')[0] + ":" + key.split(' ')[1].split(':')[1];
+                //console.log(time)
+                if(i%2 == 0)
+                    dataDict["labels"].push(time)
+                i++;
+            }
+            //console.log(dataDict)
+            i = 0;
+            for (let value of Object.values(dailyData)) {
+                //console.log(value['4. close']); 
+                if(i%2 == 0)
+                    dataDict["dataset"].push(parseFloat(value['4. close']))
+                i++;
+                // if(i == 0)
+                //     break;
+            }
+    });
+    dataDict["labels"].reverse();
+    dataDict["dataset"].reverse();
+    return dataDict;
+}
+
+async function companyIntradayChart(message, companySymbol){
+    var dict = await getIntradayData(companySymbol);
+    console.log(dict);
+    const myChart = new QuickChart();
+    myChart
+    .setConfig({
+        type: 'line',
+        data: { 
+            labels: dict["labels"], 
+            datasets: [
+                { 
+                    data: dict["dataset"],
+                    borderColor: "#c45850",
+                    label : 'closing price',
+                    fill: false
+                }
+            ]},
+        responsive: true,
+        options: {
+            scales: {
+                xAxes: [
+                  {
+                    scaleLabel: {
+                      display: true,
+                      labelString: 'Time (Hrs:Mins)',
+                      fontSize: 20,
+                    }
+                  }
+                ],
+                yAxes: [
+                  {
+                    scaleLabel: {
+                      display: true,
+                      labelString: 'Stock Price in Dollars($)',
+                    //   fontColor: '#ff0000',
+                       fontSize: 20,
+                    //   fontStyle: 'bold',
+                    }
+                  }
+                ]
+              },
+            title: {
+                display: true,
+                text: companySymbol
+            }
+        }
+    })
+    .setWidth(1200)
+    .setHeight(600)
+
+    const chartMessage = new Discord.MessageEmbed()
+        .setColor('#00FF00')
+        .setTitle(companySymbol + " (Stock Price - " + dict["metadata"][0] + ")")
         .setImage(myChart.getUrl())
         .setTimestamp()
     message.reply(chartMessage);
